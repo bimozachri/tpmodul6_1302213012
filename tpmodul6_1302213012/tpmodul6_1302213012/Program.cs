e354110b56d5c21eccecbbceaf7a9671de5d4824
Bimo Zachriansyah Wicaksono Hermawan <bimozachri@gmail.com>
2023-03-26 00:00:33 +0700

    implementasi design by contract

--- tpmodul6_1302213012/tpmodul6_1302213012/Program.cs
+++ tpmodul6_1302213012/tpmodul6_1302213012/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.Contracts;
+
 internal class Program
 {
     public class SayaTubeVideo
@@ -9,6 +11,9 @@ internal class Program
 
         public SayaTubeVideo(string title)
         {
+            Contract.Requires(title != null, "Judul video tidak boleh null");
+            Contract.Requires(title.Length <= 100, "Judul video memiliki panjang maksimal 100 karakter");
+
             this.id = new Random().Next(10000, 99999);
             this.title = title;
             this.playCount = 0;
@@ -16,7 +21,19 @@ internal class Program
 
         public void IncreasePlayCount(int playCount)
         {
-            this.playCount += playCount;
+            Contract.Requires(playCount >= 0 && playCount <= 10000000, "Input penambahan play count " +
+                "maksimal 10.000.000 per panggilan method");
+            try
+            {
+                checked
+                {
+                    this.playCount += playCount;
+                }
+            }
+            catch(OverflowException e)
+            {
+                Console.WriteLine("Error: " + e.Message);    
+            }
         }
 
         public void PrintVideoDetails()
@@ -28,9 +45,22 @@ internal class Program
 
         public static void Main(string[] args)
         {
-            SayaTubeVideo vid = new SayaTubeVideo("Tutorial Design By Contract — Bimo Zachriansyah Wicaksono Hermawan");
-            vid.IncreasePlayCount(2);
-            vid.PrintVideoDetails();
+            try
+            {
+                SayaTubeVideo vid = new SayaTubeVideo("Tutorial Design By Contract — Bimo Zachriansyah Wicaksono Hermawan");
+                vid.IncreasePlayCount(2);
+                vid.IncreasePlayCount(10000000);
+
+                for (int i = 0; i < 1000000000; i++)
+                {
+                    vid.IncreasePlayCount(1);
+                }
+                vid.PrintVideoDetails();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
         }
     }
 }
