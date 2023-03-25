8473d3cc49b0fe0a6c1d929136be4de48aea30ca
Bimo Zachriansyah Wicaksono Hermawan <bimozachri@gmail.com>
2023-03-25 23:47:56 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_1302213012/tpmodul6_1302213012/Program.cs
+++ tpmodul6_1302213012/tpmodul6_1302213012/Program.cs
@@ -1,2 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+internal class Program
+{
+    public class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            this.id = new Random().Next(10000, 99999);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int playCount)
+        {
+            this.playCount += playCount;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("ID: " +  id);
+            Console.WriteLine("Title: " + title);
+            Console.WriteLine("Play Count: " +  playCount);
+        }
+
+        public static void Main(string[] args)
+        {
+            SayaTubeVideo vid = new SayaTubeVideo("Tutorial Design By Contract — Bimo Zachriansyah Wicaksono Hermawan");
+            vid.IncreasePlayCount(2);
+            vid.PrintVideoDetails();
+        }
+    }
+}
